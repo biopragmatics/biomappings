2977bf67926092496325d0a7493066b91cc304ed
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-12-16 16:43:47 +0100

    Don't forget about incorrect mappings
    Closes #8

--- src/biomappings/tests/test_validity.py
+++ src/biomappings/tests/test_validity.py
@@ -6,11 +6,12 @@ from collections import Counter
 
 import requests
 
-from biomappings import load_mappings, load_predictions
+from biomappings import load_mappings, load_predictions, load_false_mappings
 from biomappings.utils import iterate_canonical_mappings
 
 mappings = load_mappings()
 predictions = load_predictions()
+incorrect = load_false_mappings()
 
 
 class InvalidPrefix(ValueError):
@@ -55,7 +56,7 @@ miriam_validator = MiriamValidator()
 
 def test_valid_mappings():
     """Test the validity of the prefixes and identifiers in the mappings."""
-    for mapping in itt.chain(mappings, predictions):
+    for mapping in itt.chain(mappings, incorrect, predictions):
         miriam_validator.check_valid_prefix_id(
             mapping['source prefix'],
             mapping['source identifier'],
@@ -68,7 +69,7 @@ def test_valid_mappings():
 
 def test_redundancy():
     """Test the redundancy of manually curated mappings and predicted mappings."""
-    counter = Counter(iterate_canonical_mappings(itt.chain(mappings, predictions)))
+    counter = Counter(iterate_canonical_mappings(itt.chain(mappings, incorrect, predictions)))
     redundant = [k for k, v in counter.items() if v > 1]
     if redundant:
         raise ValueError(f'Redundant: {redundant}')
