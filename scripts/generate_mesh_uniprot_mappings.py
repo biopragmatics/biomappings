96763fa4a5578c309a952fa5004b5721a0baa73f
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-12-16 15:47:08 +0100

    Update generation script

--- scripts/generate_mesh_uniprot_mappings.py
+++ scripts/generate_mesh_uniprot_mappings.py
@@ -1,30 +1,43 @@
-import re
+# -*- coding: utf-8 -*-
+
+"""Append lexical mappings between MeSH and UniProt."""
+
 import os
+import re
 from subprocess import check_output
-from indra.databases import mesh_client, hgnc_client
+
+from indra.databases import hgnc_client, mesh_client
 
 
-def get_script_url():
-    commit_hash = check_output('git rev-parse HEAD'.split()).\
-        decode('utf-8').strip()[:6]
+def get_script_url() -> str:
+    """Get the source path for this script."""
+    commit_hash = check_output('git rev-parse HEAD'.split()).decode('utf-8').strip()[:6]
     script_name = os.path.basename(__file__)
-    return (f'https://github.com/biomappings/biomappings/blob/{commit_hash}/'
-            f'scripts/{script_name}')
+    return f'https://github.com/biomappings/biomappings/blob/{commit_hash}/scripts/{script_name}'
+
+
+MESH_PROTEIN_RE = re.compile(r'^(.+) protein, human$')
 
 
 def get_mappings():
+    """Iterate high-confidence lexical mappings between MeSH and UniProt human proteins."""
     url = get_script_url()
     mapping_type = 'lexical'
     match_type = 'skos:exactMatch'
     for mesh_name, mesh_id in mesh_client.mesh_name_to_id.items():
-        match = re.match(r'^(.+) protein, human$', mesh_name)
-        if match:
-            gene_name = match.groups()[0]
-            hgnc_id = hgnc_client.get_hgnc_id(gene_name)
-            if hgnc_id:
-                uniprot_id = hgnc_client.get_uniprot_id(hgnc_id)
-                if uniprot_id and ',' not in uniprot_id:
-                    yield ('mesh', mesh_id, mesh_name,
-                           match_type,
-                           'uniprot', uniprot_id, gene_name,
-                           mapping_type, url)
+        match = MESH_PROTEIN_RE.match(mesh_name)
+        if not match:
+            continue
+        gene_name = match.groups()[0]
+        hgnc_id = hgnc_client.get_hgnc_id(gene_name)
+        if not hgnc_id:
+            continue
+        uniprot_id = hgnc_client.get_uniprot_id(hgnc_id)
+        if not uniprot_id or ',' in uniprot_id:
+            continue
+        yield (
+            'mesh', mesh_id, mesh_name,
+            match_type,
+            'uniprot', uniprot_id, gene_name,
+            mapping_type, url,
+        )
