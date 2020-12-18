2951770d6e3a266dfdda050622c8b3a9fc2a862b
Ben Gyori <ben.gyori@gmail.com>
2020-12-18 00:26:21 -0500

    Exclude primary mappings

--- scripts/import_gilda_mappings.py
+++ scripts/import_gilda_mappings.py
@@ -3,6 +3,7 @@
 """Append lexical mapping predictions from Gilda."""
 
 import csv
+import json
 import os
 from typing import Iterable, Tuple
 
@@ -24,14 +25,32 @@ db_ns_mappings = {
 }
 
 
+def get_primary_mappings():
+    """Get mappings from primary sources"""
+    from indra.resources import load_resource_json
+    mappings = set()
+    sources = ['efo', 'hp', 'doid']
+    for source in sources:
+        entries = load_resource_json(f'{source}.json')
+        for entry in entries:
+            for xref in entry.get('xrefs', []):
+                if xref['namespace'] != 'MESH':
+                    continue
+                mappings.add(('mesh', xref['id'], source, entry['id']))
+    return mappings
+
+
 def get_mappings() -> Iterable[Tuple[str, ...]]:
     """Iterate lexical mappings from Gilda."""
     url = get_script_url(__file__)
     mapping_type = 'lexical'
     match_type = 'skos:exactMatch'
     confidence = 0.95
+    primary_mappings = get_primary_mappings()
     with open(GILDA_MAPPINGS, 'r') as fh:
         for _, mesh_id, mesh_name, db_ns, db_id, db_name in csv.reader(fh, delimiter='\t'):
+            if ('mesh', mesh_id, db_ns, db_id) in primary_mappings:
+                continue
             yield (
                 'mesh', mesh_id, mesh_name,
                 match_type,
