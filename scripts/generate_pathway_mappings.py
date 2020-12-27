29d28ae9074c18b17129efe8bf1ffa09764288a0
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-12-27 18:25:59 +0100

    Update generate_pathway_mappings.py

--- scripts/generate_pathway_mappings.py
+++ scripts/generate_pathway_mappings.py
@@ -3,38 +3,28 @@
 """Generate mappings to Gilda from given PyOBO prefixes."""
 
 import itertools as itt
-from typing import Iterable, List, Tuple
+from typing import Iterable, Tuple
 
-import click
 import gilda
 import gilda.grounder
 import pyobo
+from tqdm import tqdm
 
 from biomappings.resources import append_prediction_tuples
 from biomappings.utils import get_script_url
 
 
-@click.command()
-@click.option('--prefix', dest='prefixes', multiple=True)
-def main(prefixes: List[str]):
-    """Append predictions made by Gilda for the given prefix(es)."""
-    append_prediction_tuples(itt.chain.from_iterable(
-        iter_gilda_prediction_tuples(prefix)
-        for prefix in prefixes
-    ))
-
-
-def iter_gilda_prediction_tuples(prefix: str) -> Iterable[Tuple[str, ...]]:
+def iter_gilda_prediction_tuples(prefix: str, relation: str) -> Iterable[Tuple[str, ...]]:
     """Iterate over prediction tuples for a given prefix."""
     provenance = get_script_url(__file__)
     id_name_mapping = pyobo.get_id_name_mapping(prefix)
-    for identifier, name in id_name_mapping.items():
+    for identifier, name in tqdm(id_name_mapping.items(), desc=f'Mapping {prefix}'):
         for scored_match in gilda.ground(name):
             yield (
                 prefix,
                 identifier,
                 name,
-                'skos:exactMatch',
+                relation,
                 scored_match.term.db.lower(),
                 scored_match.term.id,
                 scored_match.term.entry_name,
@@ -45,4 +35,7 @@ def iter_gilda_prediction_tuples(prefix: str) -> Iterable[Tuple[str, ...]]:
 
 
 if __name__ == '__main__':
-    main()
+    append_prediction_tuples(itt.chain.from_iterable(
+        iter_gilda_prediction_tuples(prefix, 'speciesSpecific')
+        for prefix in ['reactome', 'wikipathways']
+    ))
