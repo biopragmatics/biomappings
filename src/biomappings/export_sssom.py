c38f3de8c55ddfa62c18cc0c98d652f9a17d44dc
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-06-24 13:56:21 -0400

    Update export

--- src/biomappings/export_sssom.py
+++ src/biomappings/export_sssom.py
@@ -10,35 +10,45 @@ import pandas as pd
 from biomappings import load_mappings
 from biomappings.utils import DATA
 
-PATH = os.path.join(DATA, 'biomappings.sssom.tsv')
+PATH = os.path.join(DATA, "biomappings.sssom.tsv")
 
 
 def get_sssom_df() -> pd.DataFrame:
     """Get an SSSOM dataframe."""
     rows = []
     columns = [
-        'subject_id', 'predicate_id', 'object_id', 'subject_label', 'object_label', 'creator_id', 'license'
+        "subject_id",
+        "predicate_id",
+        "object_id",
+        "subject_label",
+        "object_label",
+        "match_type",
+        "creator_id",
+        "license",
     ]
     for mapping in load_mappings():
-        rows.append((
-            f'{mapping["source prefix"]}:{mapping["source identifier"]}',
-            f'{mapping["relation"]}',
-            f'{mapping["target prefix"]}:{mapping["target identifier"]}',
-            mapping["source name"],
-            mapping["target name"],
-            mapping["source"],
-            'CC0',  # TODO fix this with URL?
-        ))
+        rows.append(
+            (
+                f'{mapping["source prefix"]}:{mapping["source identifier"]}',
+                f'{mapping["relation"]}',
+                f'{mapping["target prefix"]}:{mapping["target identifier"]}',
+                mapping["source name"],
+                mapping["target name"],
+                "sssom:HumanCurated",  # match type
+                mapping["source"],  # curator CURIE
+                "https://creativecommons.org/publicdomain/zero/1.0/",
+            )
+        )
     return pd.DataFrame(rows, columns=columns)
 
 
 @click.command()
-@click.option('--path', default=PATH)
+@click.option("--path", default=PATH)
 def main(path):
     """Export SSSOM."""
     df = get_sssom_df()
-    df.to_csv(path, sep='\t', index=False)
+    df.to_csv(path, sep="\t", index=False)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
