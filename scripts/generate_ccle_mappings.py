17df1938370c72eda2c274dd562656d92cdc80f6
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-07-20 07:32:22 +0200

    Update generation of custom filter dict for CCLE

--- scripts/generate_ccle_mappings.py
+++ scripts/generate_ccle_mappings.py
@@ -2,7 +2,12 @@
 
 """Generate mappings to Gilda from given PyOBO prefixes."""
 
+import itertools as itt
+from collections import defaultdict
+from typing import Iterable, Mapping, Optional
+
 import click
+import networkx as nx
 import pyobo
 from more_click import verbose_option
 
@@ -15,10 +20,14 @@ from biomappings.utils import get_script_url
 def main():
     """Generate CCLE mappings."""
     provenance = get_script_url(__file__)
-    custom_filter = generate_custom_filter()
+
+    prefix = "ccle.cell"
+    targets = ["depmap", "efo", "cellosaurus", "cl", "bto"]
+
+    custom_filter = get_custom_filter(prefix, targets)
     append_gilda_predictions(
-        "ccle.cell",
-        ["depmap", "efo", "cellosaurus", "cl", "bto"],
+        prefix,
+        targets,
         provenance=provenance,
         relation="skos:exactMatch",
         custom_filter=custom_filter,
@@ -26,7 +35,48 @@ def main():
     )
 
 
-def generate_custom_filter() -> CMapping:
+def get_custom_filter(prefix: str, targets: Iterable[str]) -> Mapping[str, Mapping[str, Mapping[str, str]]]:
+    """Get a custom filter dictionary induced over the mutual mapping graph with all target prefixes.
+
+    :param prefix: The source prefix
+    :param targets: All potential target prefixes
+    :returns: A filter 3-dictionary of source prefix to target prefix to source identifier to target identifier
+    """
+    graph = mutual_mapping_graph([prefix, *targets])
+    rv = defaultdict(dict)
+    for p, identifier in graph:
+        if p != prefix:
+            continue
+        for xref_prefix, xref_identifier in nx.single_source_shortest_path(graph, (p, identifier)):
+            rv[xref_prefix][identifier] = xref_identifier
+    return {prefix: dict(rv)}
+
+
+def mutual_mapping_graph(
+    prefixes: Iterable[str],
+    skip_sources: Optional[Iterable[str]] = None,
+    skip_targets: Optional[Iterable[str]] = None
+) -> nx.Graph:
+    """Get the undirected mapping graph between the given prefixes.
+
+    :param prefixes: A list of prefixes to use with :func:`pyobo.get_filtered_xrefs` to get xrefs.
+    :param skip_sources: An optional list of prefixes to skip as the source for xrefs
+    :param skip_targets: An optional list of prefixes to skip as the target for xrefs
+    :return: The undirected mapping graph containing mappings between entries in the given namespaces.
+    """
+    prefixes = sorted(prefixes)
+    skip_sources = set() if skip_sources is None else set(skip_sources)
+    skip_targets = set() if skip_targets is None else set(skip_targets)
+    graph = nx.Graph()
+    for source, target in itt.product(prefixes, repeat=2):
+        if source == target or source in skip_sources or target in skip_targets:
+            continue
+        for source_id, target_id in pyobo.get_filtered_xrefs(source, target).items():
+            graph.add_edge((source, source_id), (target, target_id))
+    return graph
+
+
+def ccle_custom_filter() -> CMapping:
     """Get custom CCLE->X mappings."""
     ccle_depmap = pyobo.get_filtered_xrefs("ccle", "depmap")
     ccle_cellosaurus = _cdict(
