bcffffc52406833ad4565610cd7b5781f0ac5105
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-07-27 14:54:51 +0200

    Update import_compath.py

--- scripts/import_compath.py
+++ scripts/import_compath.py
@@ -8,36 +8,37 @@ from tqdm import tqdm
 
 from biomappings.resources import append_true_mappings
 
-URL = 'https://github.com/ComPath/compath-resources/raw/master/docs/data/compath.tsv'
-BLACKLIST = {'decopath', 'pathbank'}
+URL = "https://github.com/ComPath/compath-resources/raw/master/docs/data/compath.tsv"
+BLACKLIST = {"decopath", "pathbank"}
 
 
 def main():
     """Import mappings from ComPath."""
-    df = pd.read_csv(URL, sep='\t')
-    df = df[df['relation'] == 'skos:exactMatch']
-    df = df[~df['source prefix'].isin(BLACKLIST)]
-    df = df[~df['target prefix'].isin(BLACKLIST)]
-    df = df.drop_duplicates()
-    df['type'] = 'manual'
-    df['source'] = 'orcid:0000-0002-2046-6145'  # ComPath is courtesy of Uncle Daniel
+    df = pd.read_csv(URL, sep="\t")
+    df = df[df["relation"] == "skos:exactMatch"]
+    df = df[~df["source prefix"].isin(BLACKLIST)]
+    df = df[~df["target prefix"].isin(BLACKLIST)]
+    df["type"] = "manual"
+    df["source"] = "orcid:0000-0002-2046-6145"  # ComPath is courtesy of Uncle Daniel
 
     # TODO check that species are the same
 
     # Make sure nomenclature is correct
-    df['source name'] = [
-        name if prefix == 'kegg.pathway' else pyobo.get_name(prefix, identifier)
-        for prefix, identifier, name in tqdm(df[['source prefix', 'source identifier', 'source name']].values)
+    df["source name"] = [
+        name if prefix == "kegg.pathway" else pyobo.get_name(prefix, identifier)
+        for prefix, identifier, name in tqdm(
+            df[["source prefix", "source identifier", "source name"]].values
+        )
     ]
-    df['target name'] = [
-        name if prefix == 'kegg.pathway' else pyobo.get_name(prefix, identifier)
-        for prefix, identifier, name in tqdm(df[['target prefix', 'target identifier', 'target name']].values)
+    df["target name"] = [
+        name if prefix == "kegg.pathway" else pyobo.get_name(prefix, identifier)
+        for prefix, identifier, name in tqdm(
+            df[["target prefix", "target identifier", "target name"]].values
+        )
     ]
-    append_true_mappings(
-        row
-        for _, row in df.iterrows()
-    )
+    mappings = (mapping for _, mapping in df.iterrows())
+    append_true_mappings(mappings, sort=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
