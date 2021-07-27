f7c0c5f60040de4252dabba6654027da49bcc9aa
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-07-27 13:38:41 +0200

    Add compath

--- scripts/import_compath.py
+++ scripts/import_compath.py
@@ -2,63 +2,34 @@
 
 """Import mappings from ComPath."""
 
+import pandas as pd
 from tqdm import tqdm
 
 import pyobo
 from biomappings.resources import append_true_mappings
-from compath_resources import get_df
+
+URL = 'https://github.com/ComPath/compath-resources/raw/master/docs/data/compath.tsv'
 
 
 def main():
     """Import mappings from ComPath."""
-    df = get_df()
-
-    # Remove partOf relations
-    df = df[df['Mapping Type'] == 'equivalentTo']
-    del df['Mapping Type']
-
-    # Fix column names
-    df.rename(
-        columns={
-            'Source Resource': 'source prefix',
-            'Source ID': 'source identifier',
-            'Source Name': 'source name',
-            'Target Resource': 'target prefix',
-            'Target ID': 'target identifier',
-            'Target Name': 'target name',
-        },
-        inplace=True,
-    )
-
-    # Add metadata
-    df['relation'] = 'skos:exactMatch'
+    df = pd.read_csv(URL, sep='\t')
+    df = df[df['relation'] == 'skos:exactMatch']
+    df = df[df['source prefix'] != 'decopath']
+    df = df[df['target prefix'] != 'decopath']
     df['type'] = 'manual'
-    df['source'] = 'orcid:0000-0002-2046-6145' # ComPath is courtesy of Uncle Daniel
-
-    # Fix incorrect prefix for KEGG Pathways
-    for key in ('source prefix', 'target prefix'):
-        df[key] = df[key].map(lambda x: 'kegg.pathway' if x == 'kegg' else x)
-
-    # Remove prefix from KEGG Pathway identifiers
-    df['source identifier'] = [
-        identifier if prefix != 'kegg.pathway' else identifier.removeprefix('path:')
-        for prefix, identifier in tqdm(df[['source prefix', 'source identifier']].values)
-    ]
-    df['target identifier'] = [
-        identifier if prefix != 'kegg.pathway' else identifier.removeprefix('path:')
-        for prefix, identifier in tqdm(df[['target prefix', 'target identifier']].values)
-    ]
+    df['source'] = 'orcid:0000-0002-2046-6145'  # ComPath is courtesy of Uncle Daniel
 
     # TODO check that species are the same
 
     # Make sure nomenclature is correct
     df['source name'] = [
-        pyobo.get_name(prefix, identifier)
-        for prefix, identifier in tqdm(df[['source prefix', 'source identifier']].values)
+        name if prefix == 'kegg.pathway' else pyobo.get_name(prefix, identifier)
+        for prefix, identifier, name in tqdm(df[['source prefix', 'source identifier', 'source name']].values)
     ]
     df['target name'] = [
-        pyobo.get_name(prefix, identifier)
-        for prefix, identifier in tqdm(df[['target prefix', 'target identifier']].values)
+        name if prefix == 'kegg.pathway' else pyobo.get_name(prefix, identifier)
+        for prefix, identifier, name in tqdm(df[['target prefix', 'target identifier', 'target name']].values)
     ]
     append_true_mappings(
         row
