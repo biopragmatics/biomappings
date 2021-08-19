9b47629df458c1fb90b961e975e1f14cfc39946f
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-08-19 10:52:14 +0200

    Add predicted WikiPathways orthologs

---
+++ scripts/generate_wikipathways_orthologs.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+"""Generate orthologous relations between WikiPathways."""
+
+from typing import Iterable
+
+import itertools as itt
+from tqdm import tqdm
+
+import pyobo
+from biomappings.resources import PredictionTuple, append_prediction_tuples
+from biomappings.utils import get_script_url
+from gilda.process import normalize
+
+
+def _lexical_exact_match(name1: str, name2: str) -> bool:
+    return normalize(name1) == normalize(name2)
+
+
+def iterate_orthologous_lexical_matches(prefix: str = "wikipathways") -> Iterable[PredictionTuple]:
+    """Generate orthologous relations between lexical matches from different species."""
+    names = pyobo.get_id_name_mapping(prefix)
+    species = pyobo.get_id_species_mapping(prefix)
+    provenance = get_script_url(__file__)
+
+    count = 0
+    it = itt.combinations(names.items(), 2)
+    it = tqdm(it, unit_scale=True, unit="pair", total=len(names) * (len(names) - 1) / 2)
+    for (source_id, source_name), (target_id, target_name) in it:
+        if species[source_id] == species[target_id]:
+            continue
+        if _lexical_exact_match(source_name, target_name):
+            count += 1
+            yield PredictionTuple(
+                prefix,
+                source_id,
+                source_name,
+                "RO:HOM0000017",
+                prefix,
+                target_id,
+                target_name,
+                "lexical",
+                0.99,
+                provenance,
+            )
+    tqdm.write(f"Identified {count} orthologs")
+
+
+if __name__ == "__main__":
+    append_prediction_tuples(iterate_orthologous_lexical_matches())
