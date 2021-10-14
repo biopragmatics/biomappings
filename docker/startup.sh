7298926fde29bf9e290f6934b75f790eeedd9412
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-10-14 12:33:45 +0200

    Add small amount of logging to startup.sh
    The --version got added in master but should be able to print out this info
    
    The --host option should also be available

--- docker/startup.sh
+++ docker/startup.sh
@@ -2,4 +2,5 @@
 cd /biomappings
 git remote set-url origin git@github.com:$GITHUBUSER/biomappings.git
 git checkout -b curation
-biomappings web
+biomappings --version
+biomappings web --host "0.0.0.0"
