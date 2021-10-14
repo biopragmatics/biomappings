cf7a10bc040f3e94d7a105358a7ddb6f23ba0093
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-10-14 11:29:19 +0200

    Improve tests and impose QA on them
    This gives more granular messages on problematic mappings by saying which file and on which line they appear

--- tests/test_validity.py
+++ tests/test_validity.py
@@ -10,12 +10,23 @@ import bioregistry
 
 from biomappings import load_false_mappings, load_mappings, load_predictions, load_unsure
 from biomappings.resources import load_curators, mapping_sort_key
-from biomappings.utils import get_canonical_tuple, check_valid_prefix_id
+from biomappings.utils import check_valid_prefix_id, get_canonical_tuple
 
 mappings = load_mappings()
 predictions = load_predictions()
 incorrect = load_false_mappings()
 unsure = load_unsure()
+
+
+def _iter_groups():
+    for group, label in [
+        (mappings, "positive"),
+        (incorrect, "negative"),
+        (predictions, "predictions"),
+        (unsure, "unsure"),
+    ]:
+        for i, mapping in enumerate(group, start=2):
+            yield label, i, mapping
 
 
 class TestIntegrity(unittest.TestCase):
@@ -24,19 +35,38 @@ class TestIntegrity(unittest.TestCase):
     def test_canonical_prefixes(self):
         """Test that all mappings use canonical bioregistry prefixes."""
         valid_prefixes = set(bioregistry.read_registry())
-        for mapping in itt.chain(mappings, incorrect, predictions, unsure):
+        for label, line, mapping in _iter_groups():
             source_prefix, target_prefix = mapping["source prefix"], mapping["target prefix"]
-            self.assertIn(source_prefix, valid_prefixes, msg=f"Invalid prefix: {source_prefix}")
-            self.assertIn(target_prefix, valid_prefixes, msg=f"Invalid prefix: {target_prefix}")
+            self.assertIn(
+                source_prefix,
+                valid_prefixes,
+                msg=f"Invalid prefix: {source_prefix} on {label}:{line}",
+            )
+            self.assertIn(
+                target_prefix,
+                valid_prefixes,
+                msg=f"Invalid prefix: {target_prefix} on {label}:{line}",
+            )
 
     def test_normalized_identifiers(self):
         """Test that all identifiers have been normalized (based on bioregistry definition)."""
-        for mapping in itt.chain(mappings, incorrect, predictions, unsure):
-            self.assert_canonical_identifier(mapping["source prefix"], mapping["source identifier"])
-            self.assert_canonical_identifier(mapping["target prefix"], mapping["target identifier"])
+        for label, line, mapping in _iter_groups():
+            self.assert_canonical_identifier(
+                mapping["source prefix"], mapping["source identifier"], label, line
+            )
+            self.assert_canonical_identifier(
+                mapping["target prefix"], mapping["target identifier"], label, line
+            )
 
-    def assert_canonical_identifier(self, prefix: str, identifier: str) -> None:
+    def assert_canonical_identifier(
+        self, prefix: str, identifier: str, label: str, line: int
+    ) -> None:
         """Assert a given identifier is canonical.
+
+        :param prefix: The prefix to check
+        :param identifier: The identifier in the semantic space for the prefix
+        :param label: The label of the mapping file
+        :param line: The line number of the mapping
 
         .. warning::
 
@@ -50,7 +80,20 @@ class TestIntegrity(unittest.TestCase):
         self.assertIsNotNone(resource)
         norm_id = resource.normalize_identifier(identifier)
         self.assertIsNotNone(norm_id)
-        self.assertEqual(identifier, norm_id)
+        self.assertEqual(
+            identifier,
+            norm_id,
+            msg=f"Normalization of {prefix}:{identifier} failed on {label}:{line}",
+        )
+
+    def test_contributors(self):
+        """Test all contributors have an entry in the curators.tsv file."""
+        contributor_orcids = {row["orcid"] for row in load_curators()}
+        for mapping in itt.chain(mappings, incorrect, unsure):
+            source = mapping["source"]
+            if not source.startswith("orcid:"):
+                continue
+            self.assertIn(source[len("orcid:") :], contributor_orcids)
 
 
 def test_valid_mappings():
@@ -101,13 +144,3 @@ def test_unsure_sorted():
     assert unsure == sorted(  # noqa:S101
         unsure, key=mapping_sort_key
     ), "Unsure curations are not sorted"
-
-
-def test_contributors():
-    """Test all contributors have an entry in the curators.tsv file."""
-    contributor_orcids = {row["orcid"] for row in load_curators()}
-    for mapping in itt.chain(mappings, incorrect, unsure):
-        source = mapping["source"]
-        if not source.startswith("orcid:"):
-            continue
-        assert source[len("orcid:"):] in contributor_orcids
