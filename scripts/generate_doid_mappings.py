0515135cbe31a72c1d405211fa84b83eb21fc2a5
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-10-21 09:54:54 +0200

    Add Disease Ontology generation script

---
+++ scripts/generate_doid_mappings.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+"""Generate mappings to Gilda from given PyOBO prefixes."""
+
+import click
+from more_click import verbose_option
+
+from biomappings.gilda_utils import append_gilda_predictions
+from biomappings.mapping_graph import get_custom_filter
+from biomappings.utils import get_script_url
+
+
+@click.command()
+@verbose_option
+def main():
+    """Generate DOID mappings."""
+    provenance = get_script_url(__file__)
+
+    prefix = "doid"
+    targets = [
+        "umls",
+        # "ordo",
+        "efo",
+        "mondo",
+        "mesh",
+        "ido",
+        # "cido"
+    ]
+
+    custom_filter = get_custom_filter(prefix, targets)
+    append_gilda_predictions(
+        prefix,
+        targets,
+        provenance=provenance,
+        relation="skos:exactMatch",
+        custom_filter=custom_filter,
+    )
+
+
+if __name__ == "__main__":
+    main()
