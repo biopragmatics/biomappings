88bfa25d3f8fe533ad4060119bff52ce71a1dc35
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-12-14 12:58:54 -0500

    Add predictions

--- scripts/generate_agrovoc_mappings.py
+++ scripts/generate_agrovoc_mappings.py
@@ -1,22 +1,13 @@
-from rdflib import Graph
+import time
 from tqdm import tqdm
 
-import pystow
 from biomappings import PredictionTuple
 from biomappings.resources import append_prediction_tuples
 from biomappings.utils import get_script_url
 from pyobo.gilda_utils import get_grounder
-from pystow.utils import read_zipfile_rdf
+from pyobo.sources.agrovoc import ensure_agrovoc_graph
 
-
-def ensure_agrovoc(version: str) -> Graph:
-    url = f"https://agrovoc.fao.org/agrovocReleases/agrovoc_{version}_core.nt.zip"
-    path = pystow.ensure("bio", "agrovoc", version, url=url, name="core.nt.zip")
-    graph = read_zipfile_rdf(path, inner_path=f"agrovoc_{version}_core.rdf")
-    graph.bind("skosxl", "http://www.w3.org/2008/05/skos-xl")
-    return graph
-
-
+AGROVOC_VERSION = "2021-12-02"
 QUERY = """
 SELECT ?id ?label {
     ?term skosxl:prefLabel / skosxl:literalForm ?label .
@@ -29,29 +20,34 @@ SELECT ?id ?label {
 
 
 def main():
-    version = "2021-12-02"
+    """"""
     provenance = get_script_url(__file__)
     grounder = get_grounder("AGRO")
-    graph = ensure_agrovoc(version)
+    print("got grounder for AGRO", grounder)
+    t = time.time()
+    graph = ensure_agrovoc_graph(AGROVOC_VERSION)
+    print(
+        f"got RDF graph for AGROVOC {graph} with {len(graph)} triples in {time.time() - t:.2f} seconds"
+    )
     rows = []
-    for identifier_literal, label_literal in tqdm(graph.query(QUERY)):
-        identifier: str = identifier_literal.toPython()
-        name: str = label_literal.toPython()
+    for identifier, name in tqdm(graph.query(QUERY)):
         for scored_match in grounder.ground(name):
-            rows.append(PredictionTuple(
-                "agrovoc",
-                identifier,
-                name,
-                "skos:exactMatch",
-                scored_match.term.db.lower(),
-                scored_match.term.id,
-                scored_match.term.entry_name,
-                "lexical",
-                scored_match.score,
-                provenance,
-            ))
+            rows.append(
+                PredictionTuple(
+                    "agrovoc",
+                    identifier,
+                    name,
+                    "skos:exactMatch",
+                    scored_match.term.db.lower(),
+                    scored_match.term.id,
+                    scored_match.term.entry_name,
+                    "lexical",
+                    scored_match.score,
+                    provenance,
+                )
+            )
     append_prediction_tuples(rows)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
