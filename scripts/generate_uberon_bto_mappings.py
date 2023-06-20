0cdfc168aff97ba22214ef2b13e0342f536a7cd6
Benjamin M. Gyori <ben.gyori@gmail.com>
2023-06-20 10:25:46 -0400

    Add UBERON-BTO predictions (#129)
    Addresses #128. This PR adds a script to generate simple UBERON-BTO
    mappings that are at the level of exact or approximate lexical matches
    using standard names. As far as I can tell, BTO doesn't maintain its own
    cross-references to UBERON, however, UBERON already maps to 1403 BTO IDs
    which we don't redundantly predict here. The script predicts 60 missing
    mappings. If useful, it could be extended to take synonyms into account
    in addition to standard names.
    
    ---------
    
    Co-authored-by: Charles Tapley Hoyt <cthoyt@gmail.com>

---
+++ scripts/generate_uberon_bto_mappings.py
@@ -0,0 +1,59 @@
+"""Generate mappings using Gilda from UBERON to BTO."""
+
+import obonet
+from gilda import Term, make_grounder
+from gilda.process import normalize
+
+from biomappings.resources import PredictionTuple, append_prediction_tuples
+
+base = "/Users/ben/src"
+# base = "/Users/cthoyt/dev"
+uberon_graph = obonet.read_obo(f"{base}/uberon/src/ontology/uberon-edit.obo")
+bto_graph = obonet.read_obo(f"{base}/BTO/bto.obo")
+
+terms = []
+for node, data in bto_graph.nodes(data=True):
+    prefix, id_stub = node.split(":", maxsplit=1)
+    identifier = node
+    term = Term(
+        normalize(data["name"]), data["name"], prefix, identifier, data["name"], "name", "bto"
+    )
+    terms.append(term)
+
+grounder = make_grounder(terms)
+
+mappings = {}
+bto_used = set()
+for node, data in uberon_graph.nodes(data=True):
+    if not node.startswith("UBERON"):
+        continue
+    bto_refs = [xref for xref in data.get("xref", []) if xref.startswith("BTO")]
+    bto_used |= set(bto_refs)
+    if bto_refs:
+        continue
+    for match in grounder.ground(data["name"]):
+        if match.term.db == "BTO":
+            mappings[node] = match.term.id
+
+print("Found %d existing UBERON->BTO mappings." % len(bto_used))
+print("Predicted %d new UBERON->BTO mappings." % len(mappings))
+
+predictions = []
+for uberon_id, bto_id in mappings.items():
+    if bto_id in bto_used:
+        continue
+    pred = PredictionTuple(
+        source_prefix="uberon",
+        source_id=uberon_id,
+        source_name=uberon_graph.nodes[uberon_id]["name"],
+        relation="skos:exactMatch",
+        target_prefix="bto",
+        target_identifier=bto_id,
+        target_name=bto_graph.nodes[bto_id]["name"],
+        type="semapv:LexicalMatching",
+        confidence=0.9,
+        source="generate_uberon_bto_mappings.py",
+    )
+    predictions.append(pred)
+
+append_prediction_tuples(predictions, deduplicate=True, sort=True)
