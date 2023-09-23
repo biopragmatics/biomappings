8463878bc23bdaa2bb9f802579084b43932aa17c
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-09-23 16:23:47 +0200

    Ensure SSSOM file is valid (#155)
    Closes #154 by adding a check that the SSSOM file is valid (according to sssom-py)

--- src/biomappings/export_sssom.py
+++ src/biomappings/export_sssom.py
@@ -24,7 +24,6 @@ CC0_URL = "https://creativecommons.org/publicdomain/zero/1.0/"
 META = {
     "license": CC0_URL,
     "mapping_provider": "https://github.com/biopragmatics/biomappings",
-    "mapping_set_group": "biopragmatics",
     "mapping_set_description": "Biomappings is a repository of community curated and predicted equivalences and "
     "related mappings between named biological entities that are not available from primary sources. It's also a "
     "place where anyone can contribute curations of predicted mappings or their own novel mappings.",
@@ -56,16 +55,24 @@ def get_sssom_df(use_tqdm: bool = False):
     ]
     # see https://mapping-commons.github.io/sssom/predicate_modifier/
     # for more information on predicate modifiers
-    for mappings, predicate_modifier in [
-        (load_mappings(), ""),  # no predicate modifier
-        (load_false_mappings(), "Not"),
+    for label, mappings, predicate_modifier in [
+        ("positive mappings", load_mappings(), ""),  # no predicate modifier
+        ("negative mappings", load_false_mappings(), "Not"),
     ]:
-        for mapping in tqdm(mappings, unit="mapping", unit_scale=True, disable=not use_tqdm):
+        for mapping in tqdm(
+            mappings, unit="mapping", unit_scale=True, disable=not use_tqdm, desc=label
+        ):
             prefixes.add(mapping["source prefix"])
             prefixes.add(mapping["target prefix"])
             source = mapping["source"]
             if any(source.startswith(x) for x in ["orcid:", "wikidata:"]):
+                prefixes.add(source.split(":")[0])
                 creators.add(source)
+            try:
+                prefixes.add(mapping["relation"].split(":")[0])
+            except ValueError:
+                pass  # TODO make sure all relations are valid CURIEs
+
             rows.append(
                 (
                     get_curie(mapping["source prefix"], mapping["source identifier"]),
@@ -81,7 +88,13 @@ def get_sssom_df(use_tqdm: bool = False):
                 )
             )
 
-    for mapping in tqdm(load_predictions(), unit="mapping", unit_scale=True, disable=not use_tqdm):
+    for mapping in tqdm(
+        load_predictions(),
+        unit="mapping",
+        unit_scale=True,
+        disable=not use_tqdm,
+        desc="predicted mappings",
+    ):
         prefixes.add(mapping["source prefix"])
         prefixes.add(mapping["target prefix"])
         rows.append(
@@ -111,14 +124,20 @@ def sssom():
 
     # Get a CURIE map containing only the relevant prefixes
     prefix_map = {
-        prefix: formatter
-        for prefix, formatter in bioregistry.get_prefix_map().items()
-        if prefix in prefixes
+        "RO": "http://purl.obolibrary.org/obo/RO_",  # Default
     }
+    for prefix in prefixes:
+        uri_prefix = bioregistry.get_uri_prefix(prefix)
+        if uri_prefix is None:
+            raise ValueError(f"could not look up URI prefix for {prefix}")
+        prefix_map[prefix] = uri_prefix
+
     with open(META_PATH, "w") as file:
         yaml.safe_dump({"curie_map": prefix_map, "creator_id": creators, **META}, file)
 
+    from sssom.constants import DEFAULT_VALIDATION_TYPES
     from sssom.parsers import from_sssom_dataframe
+    from sssom.validators import validate
     from sssom.writers import write_json, write_owl
 
     try:
@@ -126,6 +145,9 @@ def sssom():
     except Exception as e:
         click.secho(f"SSSOM Export failed...\n{e}", fg="red")
         return
+
+    validate(msdf=msdf, validation_types=DEFAULT_VALIDATION_TYPES)
+
     click.echo("Writing JSON")
     with JSON_PATH.open("w") as file:
         msdf.metadata[
