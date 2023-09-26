214f2dd1da8c8b852cfcf403fc18474f138499d8
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-09-26 16:09:35 +0200

    Add VO-DrugBank predictions

---
+++ scripts/generate_vo_drugbank.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""Generate mappings using Gilda from VO to DrugBank."""
+
+import click
+
+from biomappings.gilda_utils import append_gilda_predictions
+from biomappings.utils import get_script_url
+
+
+@click.command()
+def main():
+    """Generate VO-DrugBank mappings."""
+    provenance = get_script_url(__file__)
+    append_gilda_predictions("vo", "drugbank", provenance=provenance)
+
+
+if __name__ == "__main__":
+    main()
