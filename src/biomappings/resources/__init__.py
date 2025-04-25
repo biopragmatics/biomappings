013bdb701ba23158c181a0ee8196a81e951830f2
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-04-25 13:43:34 +0200

    Update __init__.py

--- src/biomappings/resources/__init__.py
+++ src/biomappings/resources/__init__.py
@@ -319,8 +319,8 @@ def lint_true_mappings(*, standardize: bool = False, path: Optional[Path] = None
 def _lint_curated_mappings(path: Path, *, standardize: bool = False) -> None:
     """Lint the true mappings file."""
     mapping_list = _load_table(path)
-    mapping_list = _remove_redundant(mapping_list, standardize=standardize)
-    mappings = _replace_local_curation_source(mapping_list)
+    mappings = _remove_redundant(mapping_list, standardize=standardize)
+    mappings = _replace_local_curation_source(mappings)
     _write_helper(MAPPINGS_HEADER, mappings, path, mode="w")
 
 
