2f739fb92b7c5e429fe8638eb63dc88633397e8b
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-10-05 15:35:05 +0200

    Create nfdi_t4fs.py

---
+++ scripts/nfdi_t4fs.py
@@ -0,0 +1,6 @@
+"""Generate mappings using from VO."""
+
+from biomappings.lexical import lexical_prediction_cli
+
+if __name__ == "__main__":
+    lexical_prediction_cli(__file__, "nfdicore", "t4fs")
