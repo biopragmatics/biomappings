329ae92792aa841339ce5e453da18aba91a80b83
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-11-17 09:21:39 +0100

    Update generate_reactome_orthologs.py

--- scripts/generate_reactome_orthologs.py
+++ scripts/generate_reactome_orthologs.py
@@ -2,16 +2,38 @@
 
 import itertools as itt
 from collections import defaultdict
-from collections.abc import Callable, Iterable, Mapping
+from collections.abc import Iterable, Mapping
 
-import click
 import pyobo
-from curies import NamableReference, Reference
-from curies.vocabulary import structural_matching
+from curies import NamableReference
+from curies.vocabulary import orthologous_to, structural_matching
 from sssom_pydantic import SemanticMapping
 from tqdm import tqdm
 
 from biomappings import append_predictions, get_script_url
+
+
+def iterate_orthologs() -> Iterable[SemanticMapping]:
+    """Iterate over Reactome orthologs based on identifier matching."""
+    get_script_url(__file__)
+    names = pyobo.get_id_name_mapping("reactome")
+    parent_identifier_to_species_identifier = _get_species_to_identifiers(names)
+    count = 0
+    for identifiers in tqdm(parent_identifier_to_species_identifier.values()):
+        for source_id, target_id in itt.product(identifiers, repeat=2):
+            if source_id >= target_id:
+                continue
+            count += 1
+            yield SemanticMapping(
+                subject=NamableReference(
+                    prefix="reactome", identifier=source_id, name=names[source_id]
+                ),
+                predicate=orthologous_to,
+                object=NamableReference(
+                    prefix="reactome", identifier=target_id, name=names[target_id]
+                ),
+                justification=structural_matching,
+            )
 
 
 def _get_species_to_identifiers(names: Mapping[str, str]) -> Mapping[str, list[str]]:
@@ -21,35 +43,5 @@ def _get_species_to_identifiers(names: Mapping[str, str]) -> Mapping[str, list[s
     return species_to_identifiers
 
 
-def iterate_orthologous_lexical_matches() -> Iterable[SemanticMapping]:
-    """Generate orthologous relations between Reactome pathways."""
-    yield from iterate_orthologs("reactome", _get_species_to_identifiers)
-
-
-def iterate_orthologs(
-    prefix: str,
-    f: Callable[[Mapping[str, str]], Mapping[str, list[str]]],
-) -> Iterable[SemanticMapping]:
-    """Iterate over orthologs based on identifier matching."""
-    get_script_url(__file__)
-    names = pyobo.get_id_name_mapping(prefix)
-    parent_identifier_to_species_identifier = f(names)
-    count = 0
-    for identifiers in tqdm(parent_identifier_to_species_identifier.values()):
-        for source_id, target_id in itt.product(identifiers, repeat=2):
-            if source_id >= target_id:
-                continue
-            count += 1
-            yield SemanticMapping(
-                subject=NamableReference(
-                    prefix=prefix, identifier=source_id, name=names[source_id]
-                ),
-                predicate=Reference.from_curie("RO:HOM0000017"),
-                object=NamableReference(prefix=prefix, identifier=target_id, name=names[target_id]),
-                justification=structural_matching,
-            )
-    click.echo(f"[{prefix}] Identified {count} orthologs")
-
-
 if __name__ == "__main__":
-    append_predictions(iterate_orthologous_lexical_matches())
+    append_predictions(iterate_orthologs())
