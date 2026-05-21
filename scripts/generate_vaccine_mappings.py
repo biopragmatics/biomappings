fe4ff2b1ea3fb0cce43883b8bddcdef0aedc5d25
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-05-21 17:44:10 +0200

    Update typing

--- scripts/generate_vaccine_mappings.py
+++ scripts/generate_vaccine_mappings.py
@@ -3,6 +3,7 @@
 import click
 import pyobo
 import ssslm
+from bioregistry import NormalizedNamableReference
 from curies.vocabulary import exact_match, lexical_matching_process
 from pyobo.sources.cpt import iter_terms
 from sssom_pydantic import MappingTool, SemanticMapping
@@ -20,7 +21,9 @@ def main() -> None:
     append_lexical_predictions("cpt", ["mesh", "vo"], mapping_tool=provenance)
 
     preds = []
-    grounder: ssslm.Grounder = pyobo.get_grounder(["mesh", "vo"], versions=["2023", None])
+    grounder: ssslm.Grounder[NormalizedNamableReference] = pyobo.get_grounder(
+        ["mesh", "vo"], versions=["2023", None]
+    )
     for term in iter_terms():
         texts = [term.name, *(s.name for s in term.synonyms)]
         for text in texts:
