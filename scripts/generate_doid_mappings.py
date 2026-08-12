404505d83143737e4860b3a260a7af53f3124469
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-08-12 12:18:43 +0200

    Re-run several mapping prediction workflows (#278)

--- scripts/generate_doid_mappings.py
+++ scripts/generate_doid_mappings.py
@@ -5,7 +5,7 @@ from biomappings import lexical_prediction_cli
 if __name__ == "__main__":
     lexical_prediction_cli(
         "doid",
-        ["umls", "efo", "mesh", "mondo", "ido", "ordo", "cido"],
+        ["efo", "mesh", "mondo", "ido", "ordo", "cido"],
         filter_mutual_mappings=True,
         script=__file__,
     )
