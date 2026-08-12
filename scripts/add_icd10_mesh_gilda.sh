51e95337920cae8bb1542c1df778ebbbce879ace
Harshit Soni <soni.harshit.1903@gmail.com>
2026-08-12 13:23:42 -0400

    Modified script to append predictions to biomappings via cli arg, added shell script for gilda and modified leonmap; regenerated ICD-10 <> MeSH predictions and re-curated 26 mappings

---
+++ scripts/add_icd10_mesh_gilda.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+uv run \
+  --script https://github.com/gyorilab/mapnet/raw/refs/heads/main/scripts/generate_gilda_mesh_icd10_mapping.py --predictions-path src/biomappings/resources/predictions.sssom.tsv
--- scripts/add_icd10_mesh_leonmap.sh
+++ scripts/add_icd10_mesh_leonmap.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 uv run \
-  --script https://github.com/gyorilab/mapnet/raw/refs/heads/main/scripts/generate_leonmap_mesh_icd10_mapping.py
+  --script https://github.com/gyorilab/mapnet/raw/refs/heads/main/scripts/generate_leonmap_mesh_icd10_mapping.py --predictions-path src/biomappings/resources/predictions.sssom.tsv
