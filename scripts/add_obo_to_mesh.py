cc80edff65b116429e5bf7455be5fcef0403d31c
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-08-12 22:06:59 +0200

    Add OBO-wide MeSH curation script and predictions (#279)
    This PR adds a script that creates mappings to MeSH from _all_ OBO
    ontologies (except a small number of ones that are too big and that
    can't load). It also curates several hundred easy ones.

---
+++ scripts/add_obo_to_mesh.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env -S uv run --script
+
+# /// script
+# requires-python = ">=3.14"
+# dependencies = [
+#     "biomappings",
+#     "bioregistry>=0.14.0",
+#     "pyobo>=0.13.3",
+#     "sssom-curator",
+# ]
+#
+# [tool.uv.sources]
+# biomappings = { path = "../" }
+# sssom-curator = { path = "../../sssom-curator" }
+# pyobo = { path = "../../pyobo" }
+# ///
+
+"""Predict mappings to MeSH for all OBO foundry ontologies."""
+
+import logging
+
+import bioregistry
+
+from biomappings import lexical_prediction_cli
+
+if __name__ == "__main__":
+    skip = {"ncit", "ncbitaxon", "interpro", "chebi", "omit", "gaz"}
+    prefixes = sorted(
+        resource.prefix
+        for resource in bioregistry.resources()
+        if resource.get_obofoundry_prefix()
+        and not resource.is_deprecated()
+        and not resource.no_own_terms
+        and resource.prefix not in skip
+    )
+    logging.getLogger("pyobo").setLevel(logging.ERROR)
+    lexical_prediction_cli(
+        "mesh",
+        prefixes,
+        filter_mutual_mappings=True,
+        flip=True,
+    )
