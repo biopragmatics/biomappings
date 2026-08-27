4f4e35a5db95aa073f61891cca59378fbb055c70
Harshit Soni <soni.harshit.1903@gmail.com>
2026-08-26 21:58:22 -0400

    Updated ICD-10 <> MeSH exact-match only predictions from LeonMap and Gilda via new SeMRA version

--- scripts/add_icd10_mesh_gilda.sh
+++ scripts/add_icd10_mesh_gilda.sh
@@ -3,4 +3,5 @@
 SCRIPT_DIR=$(dirname $(realpath "$0"))
 uv run \
   --script https://github.com/gyorilab/mapnet/raw/refs/heads/main/scripts/generate_gilda_mesh_icd10_mapping.py \
-  --predictions-path "$SCRIPT_DIR/../src/biomappings/resources/predictions.sssom.tsv"
+  --predictions-path "$SCRIPT_DIR/../src/biomappings/resources/predictions.sssom.tsv" \
+  --semra-url "${SEMRA_URL:-https://zenodo.org/records/21935586/files/processed.sssom.tsv.gz?download=1}"
--- scripts/add_icd10_mesh_leonmap.sh
+++ scripts/add_icd10_mesh_leonmap.sh
@@ -3,4 +3,7 @@
 SCRIPT_DIR=$(dirname $(realpath "$0"))
 uv run \
   --script https://github.com/gyorilab/mapnet/raw/refs/heads/main/scripts/generate_leonmap_mesh_icd10_mapping.py \
-  --predictions-path "$SCRIPT_DIR/../src/biomappings/resources/predictions.sssom.tsv"
+  --predictions-path "$SCRIPT_DIR/../src/biomappings/resources/predictions.sssom.tsv" \
+  --work-dir "${WORK_DIR:-.}" \
+  --semra-url "${SEMRA_URL:-https://zenodo.org/records/21935586/files/processed.sssom.tsv.gz?download=1}" \
+  --exact-only
