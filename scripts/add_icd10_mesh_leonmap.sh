d78a75374a4d3ab66acfbd92af96a351007cc17e
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-09-01 23:15:29 +0200

    Update add_icd10_mesh_leonmap.sh

--- scripts/add_icd10_mesh_leonmap.sh
+++ scripts/add_icd10_mesh_leonmap.sh
@@ -4,6 +4,5 @@ SCRIPT_DIR=$(dirname $(realpath "$0"))
 uv run \
   --script https://github.com/gyorilab/mapnet/raw/refs/heads/main/scripts/generate_leonmap_mesh_icd10_mapping.py \
   --predictions-path "$SCRIPT_DIR/../src/biomappings/resources/predictions.sssom.tsv" \
-  --work-dir "${WORK_DIR:-.}" \
-  --semra-url "${SEMRA_URL:-https://zenodo.org/records/21935586/files/processed.sssom.tsv.gz?download=1}" \
-  --exact-only
+  --semra-url https://zenodo.org/records/21935586/files/processed.sssom.tsv.gz \
+  --coerce-exact
